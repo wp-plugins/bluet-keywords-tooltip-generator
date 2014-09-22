691328f8bef4bb0cc291507c89bd6a607004fae9
lebleut <lebleut@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-22 23:14:37 +0000

    fix fetching
    git-svn-id: https://plugins.svn.wordpress.org/bluet-keywords-tooltip-generator/tags/2.1.0@995043 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -132,8 +132,8 @@ function filter_any_content($subject_hooks){
 
 							$img=$arr['img'];
 							$dfn=$arr['dfn'];
-							
-							$cont=preg_replace('#(([\*\s\(])('.$term.')([,\.\s\*\)]))#i','$2__$3__$4',$cont,$limit_match);
+
+							$cont=preg_replace('#((\W)('.$term.')(\W))#i','$2__$3__$4',$cont,$limit_match);
 						}
 						
 						foreach($my_keywords_terms as $id=>$arr){
