5594f28125fd880e38d1fe6d12694e1e358a6f00
lebleut <lebleut@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-04 23:36:45 +0000

    v 2.4.9.5 upload
    git-svn-id: https://plugins.svn.wordpress.org/bluet-keywords-tooltip-generator/tags/2.4.9.5@1105518 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ functions.php
@@ -0,0 +1,68 @@
+<?php
+defined('ABSPATH') or die("No script kiddies please!");
+
+//common functions
+function bluet_kttg_tooltip_layout($term_title,$dfn,$img,$id,$bluet_kttg_show_glossary_link = null,$bluet_kttg_glossary_page = null){
+	global $is_kttg_glossary_page;
+//generates the HTML code of the tooltip model
+
+$kttg_title_layout='';
+
+//check if the hide title setting is checked to decide wether to show the title or not
+	$kttg_tmp_title_setting=get_option( 'bluet_kw_settings' );
+	$kttg_hide_title_setting=$kttg_tmp_title_setting['bt_kw_hide_title'];
+	$button_prop='none';
+	
+	if(is_i_devise()){
+		$button_prop='block';
+	}
+	if($kttg_hide_title_setting!='on'){
+		//dont make additional spaces inside this html tag 
+		$kttg_title_layout='<span class="bluet_title_on_block">'.$term_title.'</span>';
+	}
+	
+	$kttg_footer='';
+	if(!$is_kttg_glossary_page and $bluet_kttg_show_glossary_link=="on" and $bluet_kttg_glossary_page!=""){
+		//add a note at the footer of the tooltip	
+		$kttg_footer=$bluet_kttg_glossary_page;
+		$kttg_footer='<a href="'.$kttg_footer.'">'.__('View glossary','bluet-kw').'</a>';
+	}
+	
+		$layout_ret='<span class="bluet_block_to_show" data-tooltip="'.$id.'">'
+						.'<img src="'.plugin_dir_url(__FILE__).'assets/close.png" class="bluet_hide_tooltip_button" style="display:'.$button_prop.';"/>'
+						.'<div class="bluet_block_container">'
+							.'<div class="bluet_img_in_tooltip">'.$img.'</div>'
+							.'<div class="bluet_text_content">'
+								.$kttg_title_layout
+								.wpautop($dfn)
+							.'</div>'
+							.'<div class="bluet_block_footer">'.$kttg_footer.'</div>'
+						.'</div>'
+				.'</span>';
+				
+	return $layout_ret;
+}
+
+function kttg_length_compare( $a, $b ) {
+    return strlen($a)-strlen($b) ;
+}
+
+
+function is_i_devise(){ 
+//verify if the client is on a mac iphone ipod or ipad
+
+	$user_agent     =   $_SERVER['HTTP_USER_AGENT'];
+    $ret    =   false;
+    $os_array       =   array(
+                            '/iphone/i',
+                            '/ipod/i',
+                            '/ipad/i'
+                        );
+
+    foreach ($os_array as $regex) {
+        if (preg_match($regex, $user_agent)) {
+            $ret    =   true;
+        }
+    }   
+    return $ret;
+}
---
+++ glossary-shortcode.php
@@ -0,0 +1,112 @@
+<?php
+
+# Adds a shortcode called 'kttg_glossary'.
+add_shortcode('kttg_glossary', 'bluet_kttg_glossary');
+
+function bluet_kttg_glossary(){
+	 global $is_kttg_glossary_page;
+	 global $wpdb;
+    $is_kttg_glossary_page=true;
+	
+//Begin -- glossary permalink page option
+	if(!get_option('bluet_kttg_glossary_page')){
+		//attribute glossary page permalink
+		add_option('bluet_kttg_glossary_page',get_the_permalink());
+	}elseif(get_option('bluet_kttg_glossary_page')!=get_the_permalink()){
+		//update glossary page permalink if different
+		update_option('bluet_kttg_glossary_page',get_the_permalink());
+	}
+//End -- glossary permalink page option
+
+   $ret='<div class="kttg_glossary_header">* <span class="bluet_glossary_all"><a href="'.get_the_guid().'">'.__('ALL','bluet-kw').'</a></span> * ';
+   $chara='A';
+   for($i=0;$i<26;$i++){ 
+   
+   	   $kttg_posts_num=$wpdb->get_var($wpdb->prepare("
+												SELECT      COUNT(*)
+												FROM        $wpdb->posts
+												WHERE       SUBSTR($wpdb->posts.post_title,1,1) = %s
+													AND $wpdb->posts.post_type='my_keywords'
+													AND $wpdb->posts.post_status = 'publish'
+												ORDER BY    $wpdb->posts.post_title"
+											,$chara)); 
+		$found_letter_class='';
+		
+		if($kttg_posts_num==0){
+			$kttg_posts_num='';
+			$link_to_the_letter_page="";
+		}else{
+			$found_letter_class='bluet_glossary_found_letter';
+			$link_to_the_letter_page='href="'.get_the_guid().'&letter='.$chara.'"';
+		}
+       $ret.=' <span class="bluet_glossary_letter '.$found_letter_class.'"><a '.$link_to_the_letter_page.'>'.$chara.'<span class="bluet_glossary_letter_count">'.$kttg_posts_num.'</span></a></span>';
+       $chara++;
+   }
+   
+   $ret.='</div>';
+   
+   $postids=array();
+   
+   $chosen_letter=null;
+   if($_GET["letter"]){
+       $chosen_letter=$_GET["letter"];
+	   
+	   $postids=$wpdb->get_col($wpdb->prepare("
+												SELECT      ID
+												FROM        $wpdb->posts
+												WHERE       SUBSTR($wpdb->posts.post_title,1,1) = %s
+													AND $wpdb->posts.post_type='my_keywords'
+													AND $wpdb->posts.post_status = 'publish'
+												ORDER BY    $wpdb->posts.post_title"
+											,$chosen_letter)); 
+   }
+
+   // set the "paged" parameter (use 'page' if the query is on a static front page)
+    $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+
+    // the query
+	    $args=array(
+			'post__in'		=>$postids,
+			'post_type'     =>'my_keywords',
+            'order'         => 'ASC',
+            'orderby'       => 'title',
+			'showposts'		=>10,
+			'paged'			=>$paged
+    );
+	
+    $the_query = new WP_Query( $args); 
+							
+   // The Loop
+    ?>
+
+    <?php if ( $the_query->have_posts() ) : 
+        $ret.='<div class="kttg_glossary_content"><ul>';
+			while ( $the_query->have_posts() ) :
+                $the_query->the_post();
+
+                //echo(substr(get_the_title(),0,1).'<br>');
+                if((strtoupper(substr(get_the_title(),0,1))==$chosen_letter) or $chosen_letter==null){                    
+                    $ret.='<li style="list-style-type: none;">'.get_the_title().'</li>';
+                }
+                
+			endwhile;
+        $ret.='</ul></div>';
+	
+    // next_posts_link() usage with max_num_pages
+    $ret.=get_previous_posts_link( '<span style="">Prev --   </span>' );
+    $ret.=get_next_posts_link( '<span style="">   -- Next</span>', $the_query->max_num_pages );
+
+    ?>
+
+    <?php 
+    // clean up after our query
+    wp_reset_postdata(); 
+    ?>
+
+    <?php else:  ?>
+    <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+	<?php 
+	endif;	
+	
+   return $ret;
+}
