ebdbc92199a751309e9290efd285a91750350575
lebleut <lebleut@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-09 22:59:32 +0000

    v2.6.2 upload
    git-svn-id: https://plugins.svn.wordpress.org/bluet-keywords-tooltip-generator/tags/2.6.2@1177919 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ functions.php
@@ -0,0 +1,131 @@
+<?php
+defined('ABSPATH') or die("No script kiddies please!");
+
+//common functions
+function bluet_kttg_tooltip_layout($term_title,$dfn,$img,$id,$bluet_kttg_show_glossary_link = null,$bluet_kttg_glossary_page = null){
+	global $is_kttg_glossary_page;
+//generates the HTML code of the tooltip model
+
+$kttg_title_layout='';
+
+//check if the hide title setting is checked to decide wether to show the title or not
+	$kttg_tmp_title_setting=get_option( 'bluet_kw_settings' );
+	$kttg_hide_title_setting=$kttg_tmp_title_setting['bt_kw_hide_title'];
+	if($kttg_hide_title_setting!='on'){
+		//dont make additional spaces inside this html tag 
+		$kttg_title_layout='<span class="bluet_title_on_block">'.$term_title.'</span>';
+	}
+	
+	$kttg_footer='';
+	
+	if(!$is_kttg_glossary_page and $bluet_kttg_show_glossary_link=="on" and $bluet_kttg_glossary_page!=""){
+		//add a note at the footer of the tooltip	
+		$kttg_footer='<a href="'.$bluet_kttg_glossary_page.'">'.__('View glossary','bluet-kw').'</a>';
+	}
+	
+		$layout_ret='<span class="bluet_block_to_show" data-tooltip="'.$id.'">'
+						.'<img src="'.plugin_dir_url(__FILE__).'assets/close.png" class="bluet_hide_tooltip_button" />'
+						.'<div class="bluet_block_container">'
+							.'<div class="bluet_img_in_tooltip">'.$img.'</div>'
+							.'<div class="bluet_text_content">'
+								.$kttg_title_layout
+								.wpautop($dfn)
+							.'</div>'
+							.'<div class="bluet_block_footer">'.$kttg_footer.'</div>'
+						.'</div>'
+				.'</span>';
+				
+	return $layout_ret;
+}
+
+function kttg_length_compare( $a, $b ) {
+    return strlen($a)-strlen($b) ;
+}
+ function is_i_device(){
+	 return true;
+ }
+
+function kttg_get_related_keywords($my_post_id){
+	//return an array of related keywords of the current post
+		//delete this function for optimization !
+	
+	global $more;
+
+	//fetch terms
+	$all_kw_titles=array(); //will contains keywords names with IDs ready for preg_match
+	
+	$kw_args =array(
+		'post_type'=>'my_keywords', //to receive only keywords
+		'posts_per_page'=>-1
+	);
+	$the_kw_query=get_posts($kw_args);
+	//$the_kw_query = new WP_Query( $kw_args );	
+
+	// The Loop to get all the keywords and its syns of the site in $all_kw_titles
+		foreach($the_kw_query as $kw_post){
+			//
+			$syn=get_post_meta($kw_post->ID,'bluet_synonyms_keywords',true);
+			
+			//verify if prefix
+			$is_prefix=false;
+			$kw_after='';
+			
+			if(function_exists('bluet_prefix_metabox')){
+				if(get_post_meta($kw_post->ID,'bluet_prefix_keywords',true)){
+					$is_prefix=true;
+				}
+			}
+			
+			if($is_prefix){ $kw_after='\w*'; }
+			
+			if(!empty($syn)){
+				$syn='|'.$syn.''.$kw_after;
+			}	
+
+			//change unmatchable apostrophe			
+			//$term_title=str_replace("&#8217;","'",get_the_title());
+
+			$term_title=$kw_post->post_title;
+			$all_kw_titles[$kw_post->ID]='((\W)('.$term_title.''.$kw_after.''.$syn.')(\W))i';				
+		}
+	
+
+	/*get all posts (but not post type keywords)*/	
+	//$posttypes_to_match=array('post','page');//initial posttypes to match
+	
+	//$posttypes_to_match=apply_filters('bluet_kttg_posttypes_to_match',$posttypes_to_match);
+
+	//init the post keywords related to zero
+	$post_have_kws=array();
+	$content_to_check='';
+	//set terms for each post , it can be changed to custom fields
+	foreach($all_kw_titles as $term_id=>$term){		
+		$more=1; //to make the <!--more--> tag return the hole content of the post
+
+		//look for the $term in the content (### do something here to support custom fields)
+		$content_to_check=' '.get_post($my_post_id)->post_content;
+
+		if(function_exists('bluet_kttg_add_meta_to_check')){
+			$content_to_check.=bluet_kttg_add_meta_to_check($my_post_id);
+		}
+		
+		$content_to_check=strip_tags($content_to_check);//strip_tags eliminates HTML tags before passing in pregmatch
+
+		
+		if(preg_match($term,$content_to_check)){ 
+			$post_have_kws[]=$term_id;
+		}										
+	}
+
+	return $post_have_kws;
+}
+
+function kttg_get_related_posts($my_post_id){
+	//return an array of related posts of the current keyword
+}
+function elim_apostrophes($chaine){
+	//pour éliminé les apostrophes unicode echanger par apostrophe ascii	
+	$resultat=str_replace("&#8217;","'",$chaine);
+
+	return $resultat;
+}
---
+++ keyword-posttype.php
@@ -0,0 +1,110 @@
+<?php
+defined('ABSPATH') or die("No script kiddies please!");
+
+if ( ! defined( 'BT_KW_BASE_FILE' ) )
+    define( 'BT_KW_BASE_FILE', __FILE__ );
+if ( ! defined( 'BT_KW_BASE_DIR' ) )
+    define( 'BT_KW_BASE_DIR', dirname( BT_KW_BASE_FILE ) );
+if ( ! defined( 'BT_KW_PLUGIN_URL' ) )
+    define( 'BT_KW_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+
+class bluet_keyword{
+	
+	function __construct(){
+		$this->register_my_post_type();
+		
+		$this->add_columns();
+	}
+	
+	public function register_my_post_type(){
+	global $bluet_kw_capability;
+
+		$args=array(
+			'labels'=>array(
+				'name'=>__('KeyWords','bluet-kw'),
+				'singular_name'=>__('KeyWord','bluet-kw'),
+				'menu_name'=>__('My KeyWords','bluet-kw'),
+				'name_admin_bar'=>__('My KeyWords','bluet-kw'),
+				'all_items'=>__('All my KeyWords','bluet-kw'),
+				'add_new' =>__('Add new one','bluet-kw'),
+				'add_new_item'=>__('New KeyWord','bluet-kw'),
+				'edit_item'=>__('Edit KeyWord','bluet-kw'),
+				'new_item'=>__('New KeyWord','bluet-kw'),
+				'view_item'=>__('View KeyWord','bluet-kw'),
+				'search_items'=>__('Search for KeyWords','bluet-kw'),
+				'not_found'=>__('KeyWords not found','bluet-kw'),
+				'not_found_in_trash'=>__('KeyWords not found in trash','bluet-kw'),
+				'parent_item_colon' =>__('Parent KeyWords colon','bluet-kw')
+				),
+			'public'=>true,
+			'supports'=>array('title','editor','thumbnail'),
+			'menu_icon'=>plugins_url('assets/ico_16x16.png',__FILE__),
+			
+		);
+		
+		//modify capabilities if bluet_kw_capability hook has been called
+		
+		
+		if($bluet_kw_capability!='manage_options'){
+		$args['capabilities']=array(
+		        'edit_post' => $bluet_kw_capability,
+				'edit_posts' => $bluet_kw_capability,
+				'publish_posts' => $bluet_kw_capability,
+				'delete_post' => $bluet_kw_capability,
+			);
+			
+		}		
+	
+		register_post_type('my_keywords',$args);		
+	}
+	public function add_columns(){
+		
+		// add the picture among columns
+		add_filter('manage_my_keywords_posts_columns', function($defaults){		
+
+			$defaults['the_picture']=__('Picture','bluet-kw');
+			$defaults['is_prefix'] =__('Is Prefix ?','bluet-kw');
+			$defaults['is_video'] =__('Video tooltip','bluet-kw');
+			
+			//we want to rearrange the columns apearance
+			$reArr['cb']=$defaults['cb']; //checkBox column
+			$reArr['the_picture']=$defaults['the_picture'];
+			$reArr['title']=$defaults['title'];
+			
+			//is prefix ? if appropriate addon is activated
+			if(function_exists('bluet_prefix_metabox')){
+				$reArr['is_prefix']=$defaults['is_prefix'];
+			}
+			
+			if(function_exists('bluet_video_metabox')){
+				$reArr['is_video']=$defaults['is_video'];
+			}
+			//
+			$reArr['date']=$defaults['date'];
+			
+			//return the rearranged array
+			return $reArr;
+		});
+		
+		add_action('manage_my_keywords_posts_custom_column', function($column_name,$post_id){
+
+			if ($column_name == 'the_picture') {
+				// show content of 'directors_name' column
+				the_post_thumbnail(array(75,75));
+			}elseif($column_name == 'is_prefix'){
+				//if appropriate addon is activated
+				if(function_exists('bluet_show_prefix_in_column')){
+					bluet_show_prefix_in_column();
+				}
+			}elseif($column_name == 'is_video'){
+				//if appropriate addon is activated
+				if(function_exists('bluet_show_video_in_column')){
+					bluet_show_video_in_column();
+				}
+			}
+		},10,2); //10 priority, 2 arguments
+
+	}
+}
+
+?>
